9d248bba0a1b82a8720382069e58d49514ac8910
John Paul Cas <casjp.app@gmail.com>
2021-02-06 17:29:44 +0800

    📌 add dependency for room database and todoDao

--- app/src/main/java/com/learn/todoflow/di/AppModule.kt
+++ app/src/main/java/com/learn/todoflow/di/AppModule.kt
@@ -1,8 +1,31 @@
 package com.learn.todoflow.di
 
+import android.app.Application
+import androidx.room.Room
+import com.learn.todoflow.db.TodoDb
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Singleton
+
 /**
  * @author johnpaulcas
  * @since 06/02/2021
  */
-class AppModule {
+@Module
+@InstallIn(ApplicationComponent::class)
+object AppModule {
+
+    @Provides
+    fun provideTodoDb(
+        app: Application
+    ) = Room.databaseBuilder(app, TodoDb::class.java, "todo_database")
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideTodoDao(db: TodoDb) = db.todoDao()
+
 }
